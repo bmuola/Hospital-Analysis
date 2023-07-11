68558d7ff5bd0cdd395fd0204d95d674547d6c18
Ben <85036208+bmuola@users.noreply.github.com>
2023-07-11 12:27:15 +0300

    Update Results.sql

--- Data-overview/Results.sql
+++ Data-overview/Results.sql
@@ -1,1 +1,65 @@
+To count number of distinct patients in the dataset:
+
+| Output |
+|--------|
+| 554    |
+
+To find the average number of measurements recorded per user:
+
+| Output |
+|--------------------------------|
+| 79.2256317689530686 |
+
+To find the median number of measurements per user:
+
+| Output |
+|--------|
+|   2    |
+
+To count the number of users with 3 or more measurements:
+
+| Output |
+|--------|
+|  209   |
+
+To count the number of users with more than 1000 measurements:
+
+| Output |
+|--------|
+|   5    |
+
+To count the number of users who have logged blood glucose measurements:
+
+| Output |
+|--------|
+|  325   |
+
+To count the number of users who have at least two types of measurements:
+
+| Output |
+|--------|
+|  204   |
+
+To count the number of users who have all three measures (blood glucose, weight, and blood pressure) recorded:
+
+| Output |
+|--------|
+|  2782  |
+
+To find the median values for systolic and diastolic blood pressure:
+
+| Measurement   | Median Value |
+|---------------|--------------|
+| Systolic BP   |    126       |
+| Diastolic BP  |    79        |
+
+
+
+
+
+
+
+
+
+
 
